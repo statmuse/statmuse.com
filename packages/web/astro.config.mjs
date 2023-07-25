91e6e5ded7d0e04758af60eb032bcc99309aa8df
Eric Perez <ericricardoperez@gmail.com>
2023-07-25 00:00:09 -0700

    create redirect astro components

--- packages/web/astro.config.mjs
+++ packages/web/astro.config.mjs
@@ -10,24 +10,6 @@ export default defineConfig({
   adapter: aws(),
   experimental: { assets: true },
   // image: { service: sharpImageService() },
-  redirects: {
-    '/alexa': 'https://www.amazon.com/StatMuse-Inc/dp/B074VD131M',
-    '/company': '/company/about',
-    '/company/products': '/company/about',
-    '/company/shoutouts': '/company/about',
-    '/company/twitter-love': '/company/about',
-    '/product/data': '/product/data/nba',
-    '/product/examples': '/product/examples/nba',
-    '/getting-started/data-coverage': '/product/data',
-    '/getting-started/glossary': '/product/data',
-    '/getting-started/personalities': '/product/examples',
-    '/getting-started/voices': '/product/examples',
-    '/product/data-coverage': '/product/data',
-    '/product/glossary': '/product/data',
-    '/product/personalities': '/product/examples',
-    '/product/voices': '/product/examples',
-    '/statlove': '/company/about',
-  },
   vite: {
     optimizeDeps: {
       esbuildOptions: {
