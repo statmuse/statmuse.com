531011bb0d2310569c6a20191d11124a44614369
Eric Perez <ericricardoperez@gmail.com>
2023-07-25 23:17:45 -0700

    get asks for individual leagues

--- packages/core/src/asks.ts
+++ packages/core/src/asks.ts
@@ -1,15 +1,26 @@
 import { db } from "./db";
-import type { InferResult } from "kysely";
 
+const leagues = ['nba', 'nfl', 'nhl', 'mlb', 'pga']
 
-export const getAsksIndex = db.selectFrom('asks')
+export const getListContextIds = async (names: string[]) => {
+  const records = await db.selectFrom('contexts')
+    .where('contexts.name', 'in', names)
+    .selectAll()
+    .execute()
+  return records.map(r => r.id)
+}
+
+export const getAsksIndex = async (names = leagues) => 
+  db.selectFrom('asks')
   .selectAll('asks')
   .innerJoin('contexts', 'contexts.id', 'asks.context_id')
   .where('is_in_index', '=', true)
   .where('last_web_search_at', 'is not', null)
+  .where('context_id', 'in', await getListContextIds(names))
   .orderBy('last_web_search_at', 'desc')
   .selectAll('asks')
   .select('contexts.name as league_name')
   .limit(15)
+  .execute()
 
-export type Ask = InferResult<typeof getAsksIndex>[number]
+export type Ask = Awaited<ReturnType<typeof getAsksIndex>>[number]
