83369ceda01c818ab3c351af73764311ed1c65d3
adamelmore <elmore.adam@gmail.com>
2023-07-27 12:04:47 -0500

    feat: cool astro features

--- packages/web/astro.config.mjs
+++ packages/web/astro.config.mjs
@@ -1,6 +1,6 @@
 import tailwind from '@astrojs/tailwind'
 import aws from 'astro-sst/lambda'
-import { defineConfig, sharpImageService } from 'astro/config'
+import { defineConfig } from 'astro/config'
 import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
 
 // https://astro.build/config
@@ -8,8 +8,9 @@ export default defineConfig({
   integrations: [tailwind({ config: { applyBaseStyles: false } })],
   output: 'server',
   adapter: aws(),
-  experimental: { assets: true },
-  // image: { service: sharpImageService() },
+  compressHTML: true,
+  build: { inlineStylesheets: 'auto' },
+  experimental: { assets: true, viewTransitions: true },
   vite: {
     optimizeDeps: {
       esbuildOptions: {
