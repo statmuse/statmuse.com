91cec5b5545ad44f0070aeb53d2cfbb1c48fbc91
perez-e <ericricardoperez@gmail.com>
2023-07-27 06:48:10 -0700

    Trending Pages (#9)
    * base astro template for trending questions
    
    * define ask table interface
    
    * define context interface
    
    * query table for trending asks
    
    * render items in trendings page
    
    * get asks for individual leagues
    
    * use pagination query params to query asks
    
    * render pagination ui links
    
    * fix where clause
    
    * abstract away entire trendings page
    
    * render league specific trending page
    
    * render All trending searches
    
    * render trending page for fantasy
    
    * add finance asks table interface
    
    * create fn the queries finance asks in index
    
    * adapt page component to also handle FinanceAsks type
    
    * render money index page
    
    * fix bug
    
    * autosave formatting
    
    * simplify guard fn
    
    * hyperling things in trending page
    
    * use formate date helper for timestamp
    
    * add meta tag details
    
    * add trending link to home page

---
+++ packages/core/src/asks.ts
@@ -0,0 +1,81 @@
+import { db } from "./db";
+
+const leagues = ['nba', 'nfl', 'nhl', 'mlb', 'pga']
+
+export const getListContextIds = async (names: string[]) => {
+  const records = await db.selectFrom('contexts')
+    .where('contexts.name', 'in', names)
+    .selectAll()
+    .execute()
+  return records.map(r => r.id)
+}
+
+export const ASK_LIMIT = 15
+
+export const getAsksIndex = async (names = leagues, params: { n?: Date, p?: Date, page?: string }, isFantasy = false) => {
+  let query = db.selectFrom('asks')
+    .selectAll('asks')
+    .innerJoin('contexts', 'contexts.id', 'asks.context_id')
+    .where('is_in_index', '=', true)
+    .where('last_web_search_at', 'is not', null)
+    .where('context_id', 'in', await getListContextIds(names))
+
+  if (isFantasy) {
+    query = query.where('is_fantasy_query', '=', true)
+  }
+
+  if (params.n) {
+    query = query.where('last_web_search_at', '<', params.n)
+      .orderBy('last_web_search_at', 'desc')
+  } else if (params.p) {
+    query = query.where('last_web_search_at', '>', params.p)
+      .orderBy('last_web_search_at', 'asc')
+  } else if (params.page && params.page === 'last') {
+    query = query.orderBy('last_web_search_at', 'asc')
+  } else {
+    query = query.orderBy('last_web_search_at', 'desc')
+  }
+
+  const records = await query.selectAll('asks')
+    .select('contexts.name as league_name')
+    .limit(ASK_LIMIT + 1)
+    .execute()
+
+  if (params.p || params.page) {
+    records.reverse()
+  }
+  return records
+}
+
+export type Ask = Awaited<ReturnType<typeof getAsksIndex>>[number]
+
+export const getFinanceAsksIndex = async (params: { n?: Date, p?: Date, page?: string }) => {
+  let query = db.selectFrom('finance_asks')
+    .selectAll('finance_asks')
+    .where('is_in_index', '=', true)
+    .where('last_web_search_at', 'is not', null)
+
+
+  if (params.n) {
+    query = query.where('last_web_search_at', '<', params.n)
+      .orderBy('last_web_search_at', 'desc')
+  } else if (params.p) {
+    query = query.where('last_web_search_at', '>', params.p)
+      .orderBy('last_web_search_at', 'asc')
+  } else if (params.page && params.page === 'last') {
+    query = query.orderBy('last_web_search_at', 'asc')
+  } else {
+    query = query.orderBy('last_web_search_at', 'desc')
+  }
+
+  const records = await query.selectAll('finance_asks')
+    .limit(ASK_LIMIT + 1)
+    .execute()
+
+  if (params.p || params.page) {
+    records.reverse()
+  }
+  return records
+}
+
+export type FinanceAsk = Awaited<ReturnType<typeof getFinanceAsksIndex>>[number]
--- packages/core/src/db.ts
+++ packages/core/src/db.ts
@@ -48,6 +48,39 @@ interface UserTable {
   updated_at: ColumnType<Date, string | undefined, string | undefined>
 }
 
+interface AskTable {
+  id: Generated<string>
+
+  answer: GameraResponse
+  answer_html: string | null
+  answer_text: string | null
+  answer_type: 'answer' | 'error' | 'prompt' | 'player_bio' | 'team_franchise_bio' | 'team_season_bio' | 'unknown'
+  context_id: string
+  count_total: number
+  count_web_search: number
+  count_web_view: number
+  hex_background: string | null
+  hex_foreground:  string | null
+  image_url: string | null
+  is_fantasy_query: boolean
+  is_game_odds_query: boolean | null
+  is_in_index: boolean
+  is_in_index_pin: boolean | null
+  is_in_suggests: boolean
+  is_in_suggests_pin: boolean | null
+  is_legacy_answer: boolean
+  last_user_id: string | null
+  last_visitor_id: string | null
+  last_web_search_at: ColumnType<Date, string | undefined, string | undefined> | null
+  query: string
+  resource_path: string
+  resource_query: string
+  sid: Generated<number>
+
+  inserted_at: ColumnType<Date, string | undefined, string | undefined>
+  updated_at: ColumnType<Date, string | undefined, string | undefined>
+}
+
 interface AskUserTable {
   id: Generated<string>
 
@@ -58,10 +91,16 @@ interface AskUserTable {
   updated_at: ColumnType<Date, string | undefined, string | undefined>
 }
 
+
 interface AskEventTable {
   id: Generated<string>
 
+  conversation_token: string
+  query_raw: string
+  ask_id: string
+  question_id: string | null
   user_id: string | null
+  visitor_id: string | null
 
   inserted_at: ColumnType<Date, string | undefined, string | undefined>
   updated_at: ColumnType<Date, string | undefined, string | undefined>
@@ -154,6 +193,13 @@ interface LeagueTable {
   updated_at: ColumnType<Date, string | undefined, string | undefined>
 }
 
+interface ContextTable {
+  id: Generated<string>
+  name: string
+  inserted_at: ColumnType<Date, string | undefined, string | undefined>
+  updated_at: ColumnType<Date, string | undefined, string | undefined>
+}
+
 interface PlayerTable {
   id: Generated<string>
 
@@ -171,10 +217,36 @@ interface PlayerTable {
   updated_at: ColumnType<Date, string | undefined, string | undefined>
 }
 
+interface FinanceAskTable {
+  id: Generated<string>
+
+  answer: GameraResponse
+  answer_text: string | null
+  answer_type: 'answer' | 'asset' | 'error' | 'unknown'
+  count_total: number
+  count_web_search: number
+  count_web_view: number
+  is_in_index: boolean | null
+  is_in_index_pin: boolean | null
+  is_in_suggests: boolean | null
+  is_in_suggests_pin: boolean | null
+  last_user_id: string | null
+  last_visitor_id: string | null
+  last_web_search_at: ColumnType<Date, string | undefined, string | undefined> | null
+  query: string
+  sid: Generated<number> 
+
+  inserted_at: ColumnType<Date, string | undefined, string | undefined>
+  updated_at: ColumnType<Date, string | undefined, string | undefined>
+}
+
 export interface Database {
   users: UserTable
+  asks: AskTable
   ask_events: AskEventTable
   asks_users: AskUserTable
+  contexts: ContextTable
+  finance_asks: FinanceAskTable
   musings: MusingTable
   questions: QuestionTable
   examples: ExampleTable
