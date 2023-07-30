363b91edfe6ff7afe8fcb30c41ee05aed8374f73
Eric Perez <ericricardoperez@gmail.com>
2023-07-30 16:41:48 -0700

    innerjoin with links to include short code

--- packages/core/src/musings.ts
+++ packages/core/src/musings.ts
@@ -1,12 +1,12 @@
-import { db } from "./db"
-import { InferResult } from "kysely"
+import { db } from './db'
+import { InferResult } from 'kysely'
 
 export const getMusing = (musing?: string) =>
   db
-    .selectFrom("musings")
-    .innerJoin("questions", "questions.id", "musings.question_id")
-    .innerJoin("leagues", "leagues.id", "musings.league_id")
-    .where("musings.friendly_id", "=", musing || null)
+    .selectFrom('musings')
+    .innerJoin('questions', 'questions.id', 'musings.question_id')
+    .innerJoin('leagues', 'leagues.id', 'musings.league_id')
+    .where('musings.friendly_id', '=', musing || null)
     .selectAll()
     .limit(1)
 
@@ -14,16 +14,18 @@ const getMusingQuery = getMusing()
 export type Musing = InferResult<typeof getMusingQuery>[number]
 
 export const listLatestMusings = db
-  .selectFrom("musings")
-  .where("musings.content_type", "=", "latest-stats")
+  .selectFrom('musings')
+  .innerJoin('links', 'links.musing_id', 'musings.id')
+  .where('musings.content_type', '=', 'latest-stats')
   .where((eb) =>
     eb.or([
-      eb.cmpr("musings.publish_at", "=", null),
-      eb.cmpr("musings.publish_at", "<=", new Date()),
+      eb.cmpr('musings.publish_at', '=', null),
+      eb.cmpr('musings.publish_at', '<=', new Date()),
     ])
   )
   .limit(20)
-  .orderBy("musings.publish_at", "desc")
-  .selectAll()
+  .orderBy('musings.publish_at', 'desc')
+  .selectAll('musings')
+  .select('links.short_code')
 
 export type LatestMusing = InferResult<typeof listLatestMusings>[number]
