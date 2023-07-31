7f9e1a3e02d94a1d77d0103bc5ebe48d931cb9e2
Eric Perez <ericricardoperez@gmail.com>
2023-07-30 17:02:53 -0700

    add SHORT_LINK_URL env var

--- packages/web/src/env.d.ts
+++ packages/web/src/env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly PUBLIC_API_URL: string
   readonly GAMERA_API_URL: string
   readonly KANEDAMA_API_URL: string
+  readonly SHORT_LINK_URL: string
 }
 
 interface ImportMeta {
