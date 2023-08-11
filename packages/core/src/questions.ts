c807ed5fbdd2f7620182b7850c4dcbb43631265f
Eric Perez <ericricardoperez@gmail.com>
2023-08-10 17:14:59 -0700

    helper to query question by id or friendly id

--- packages/core/src/questions.ts
+++ packages/core/src/questions.ts
@@ -1,9 +1,35 @@
-import { InferResult } from "kysely"
-import { db } from "./db"
+import { InferResult } from 'kysely'
+import { db } from './db'
 
 export const getQuestion = (id: string) =>
-  db.selectFrom("questions").where("questions.id", "=", id).selectAll().limit(1)
+  db.selectFrom('questions').where('questions.id', '=', id).selectAll().limit(1)
 
-const getQuestionQuery = getQuestion("")
-export type Question = InferResult<typeof getQuestionQuery>[number]
-export type Answer = Question["answer"]
+const isUUID = (s: string) =>
+  new RegExp(
+    '^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$'
+  ).test(s)
+
+export const getQuestionByIdOrFriendlyId = (id: string) => {
+  let query = db
+    .selectFrom('questions')
+    .innerJoin('contexts', 'contexts.id', 'questions.context_id')
+
+  if (isUUID(id)) {
+    query = query.where(({ cmpr, or }) =>
+      or([
+        cmpr('questions.id', '=', id),
+        cmpr('questions.friendly_id', '=', id),
+      ])
+    )
+  } else {
+    query = query.where('questions.friendly_id', '=', id)
+  }
+
+  return query
+    .selectAll('questions')
+    .select('contexts.name as domain')
+    .executeTakeFirst()
+}
+
+export type Question = InferResult<ReturnType<typeof getQuestion>>[number]
+export type Answer = Question['answer']
