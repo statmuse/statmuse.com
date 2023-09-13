14815dd53b5dcd9a459f2d096ead98292078fb5d
adamelmore <elmore.adam@gmail.com>
2023-09-13 12:56:59 -0500

    chore: cleanup

--- packages/web/src/middleware.ts
+++
@@ -1,8 +0,0 @@
-// @ts-ignore
-export async function onRequest(context, next) {
-  context.locals.foo = 'bar'
-  console.log(new Map(context.request.headers))
-  console.log(JSON.stringify(context.locals, undefined, 2))
-  const response = await next()
-  return response
-}
