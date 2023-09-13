4b816bd8345ba6e83ce3e1e487554b43d8b855c4
adamelmore <elmore.adam@gmail.com>
2023-09-13 10:59:25 -0500

    fix: astro middleware

--- packages/web/src/middleware.ts
+++ packages/web/src/middleware.ts
@@ -1,5 +1,6 @@
 // @ts-ignore
 export async function onRequest(context, next) {
-  console.log(JSON.stringify(context, undefined, 2))
+  console.log(JSON.stringify(context.cookies, undefined, 2))
+  console.log(JSON.stringify(context.request, undefined, 2))
   return next()
 }
