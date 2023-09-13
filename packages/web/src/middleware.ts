03a33b63f335523e30da4fed84266705683f1e74
adamelmore <elmore.adam@gmail.com>
2023-09-13 11:12:22 -0500

    fix: more logging

--- packages/web/src/middleware.ts
+++ packages/web/src/middleware.ts
@@ -1,5 +1,7 @@
 // @ts-ignore
 export async function onRequest(context, next) {
+  const keys = Object.keys(context)
+  console.log(JSON.stringify(keys))
   console.log(JSON.stringify(context.cookies, undefined, 2))
   console.log(JSON.stringify(context.request, undefined, 2))
   return next()
