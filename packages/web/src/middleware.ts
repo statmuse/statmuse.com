b7643aa5995fe87ea209eadda1f895d49421f01e
adamelmore <elmore.adam@gmail.com>
2023-09-13 11:29:20 -0500

    fix: more logging

--- packages/web/src/middleware.ts
+++ packages/web/src/middleware.ts
@@ -1,8 +1,9 @@
 // @ts-ignore
 export async function onRequest(context, next) {
+  const response = await next()
   const keys = Object.keys(context).filter((k) => k !== 'clientAddress')
   for (const key of keys) {
     console.log(key, JSON.stringify(context[key], undefined, 2))
   }
-  return next()
+  return response
 }
