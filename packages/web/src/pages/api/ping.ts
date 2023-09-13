ff15523f127bb87b305c380893c25f9c01c7474c
adamelmore <elmore.adam@gmail.com>
2023-09-13 11:57:31 -0500

    chore: test api route context

--- packages/web/src/pages/api/ping.ts
+++ packages/web/src/pages/api/ping.ts
@@ -1,7 +1,11 @@
 import type { APIRoute } from 'astro'
 
 export const GET: APIRoute = (context) => {
-  console.log(JSON.stringify(context, undefined, 2))
+  const keys = Object.keys(context).filter((k) => k !== 'clientAddress')
+  for (const key of keys) {
+    // @ts-ignore
+    console.log(key, JSON.stringify(context[key], undefined, 2))
+  }
 
   return new Response(
     JSON.stringify({
