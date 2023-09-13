d84c6d6919cb255281cb2156c9ed6dba21df4945
adamelmore <elmore.adam@gmail.com>
2023-09-13 12:43:07 -0500

    fix: more logging

--- packages/web/src/middleware.ts
+++ packages/web/src/middleware.ts
@@ -1,7 +1,7 @@
 // @ts-ignore
 export async function onRequest(context, next) {
+  context.locals.foo = 'bar'
   console.log(new Map(context.request.headers))
-  console.log(new Map(context.cookies))
   console.log(new Map(context.locals))
   const response = await next()
   return response
