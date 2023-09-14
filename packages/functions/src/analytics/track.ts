e22d09b64799700d3eacb31fa227f5b3bb9e5945
Eric Perez <ericricardoperez@gmail.com>
2023-09-13 17:10:38 -0700

    analytics.track Pageview event

--- packages/functions/src/analytics/track.ts
+++ packages/functions/src/analytics/track.ts
@@ -1,14 +1,48 @@
-import { ApiHandler, useBody, useCookies } from 'sst/node/api'
+import { ApiHandler, useJsonBody, useCookies, useHeaders } from 'sst/node/api'
 import { Config } from 'sst/node/config'
-import { Analytics } from '@segment/analytics-node'
+import { Analytics, TrackParams } from '@segment/analytics-node'
 
-const analytics = new Analytics({ writeKey: Config.SEGMENT_WRITE_KEY })
+const analytics = process.env.IS_LOCAL
+  ? { track: (x: TrackParams) => console.log(x) }
+  : new Analytics({ writeKey: Config.SEGMENT_WRITE_KEY })
+
+// get from session & cookies
+const anonymousId = ''
+const visitor_id = ''
+const user_id = ''
+const userId = ''
+
+const flattenObj = (key: string, obj?: {}): {} => {
+  return !obj
+    ? {}
+    : Object.entries(obj).reduce((x, [k, v]) => {
+        return {
+          ...x,
+          [`${key}.${k}`]: v,
+        }
+      }, {})
+}
 
 export const handler = ApiHandler(async (_evt) => {
-  console.log('analtics.track Pageview')
-  const body = useBody()
-  console.log(body)
-  console.log(useCookies())
+  const { context, disposition, tokenizationScore, ...props } = useJsonBody()
+  const headers = useHeaders()
+  analytics.track({
+    userId,
+    anonymousId,
+    event: 'Pageview',
+    context: {
+      ip: headers['x-forwarded-for'],
+      userAgent: headers['user-agent'],
+      ...context,
+    },
+    properties: {
+      ...props,
+      ...flattenObj('disposition', disposition),
+      ...flattenObj('tokenizationScore', tokenizationScore),
+      visitor_id,
+      user_id,
+    },
+  })
   return {
     statusCode: 200,
     body: 'ok',
