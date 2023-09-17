6c870e1fadb8a106d901f01285ceca8aa318dce8
Adam Elmore <elmore.adam@gmail.com>
2023-09-17 18:25:17 -0500

    feat: stripe subscription flow (#35)
    * deps: update sst and astro
    
    * feat: payment flow

--- packages/web/src/middleware.ts
+++ packages/web/src/middleware.ts
@@ -1,5 +1,7 @@
 import * as Session from '@lib/session'
 import { defineMiddleware } from 'astro:middleware'
+import * as User from '@statmuse/core/user'
+import * as Visitor from '@statmuse/core/visitor'
 
 export const onRequest = defineMiddleware(async (context, next) => {
   const locals = context.locals
@@ -7,9 +9,24 @@ export const onRequest = defineMiddleware(async (context, next) => {
   if (!session || session.type === 'public')
     session = await Session.create(context)
 
-  const { type, properties } = session
-  // @ts-expect-error
-  locals.session = { type, ...properties }
+  const user =
+    session.type === 'user'
+      ? await User.fromEmail(session.properties.email)
+      : undefined
+
+  const visitorId =
+    session.type === 'visitor'
+      ? session.properties.id
+      : session.type === 'user'
+      ? session.properties.visitorId
+      : undefined
+
+  const visitor = await Visitor.get(visitorId!)
+  if (!visitor) throw new Error('No visitor found')
+
+  locals.session = session
+  locals.user = user
+  locals.visitor = visitor
 
   return next()
 })
