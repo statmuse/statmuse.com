420705d9b75e010706f6e713e99715d6992a69b1
Eric Perez <ericricardoperez@gmail.com>
2023-09-19 00:08:10 -0700

    get user asks and user finance asks

--- packages/core/src/asks.ts
+++ packages/core/src/asks.ts
@@ -97,3 +97,85 @@ export const getFinanceAsksIndex = async (params: {
 }
 
 export type FinanceAsk = Awaited<ReturnType<typeof getFinanceAsksIndex>>[number]
+
+export const getUserAsks = async (
+  userId: string,
+  params: { n?: Date; p?: Date; page?: string }
+) => {
+  let query = db
+    .selectFrom('asks_users')
+    .innerJoin('asks', 'asks.id', 'asks_users.ask_id')
+    .innerJoin('contexts', 'contexts.id', 'asks.context_id')
+    .where('asks.is_in_index', '=', true)
+    .where('asks_users.user_id', '=', userId)
+
+  if (params.n) {
+    query = query
+      .where('asks_users.last_asked_at', '<', params.n)
+      .orderBy('asks_users.last_asked_at', 'desc')
+  } else if (params.p) {
+    query = query
+      .where('asks_users.last_asked_at', '>', params.p)
+      .orderBy('asks_users.last_asked_at', 'asc')
+      .offset(1)
+  } else if (params.page && params.page === 'last') {
+    query = query.orderBy('asks_users.last_asked_at', 'asc')
+  } else {
+    query = query.orderBy('asks_users.last_asked_at', 'desc')
+  }
+
+  const records = await query
+    .selectAll('asks')
+    .select('asks_users.last_asked_at as last_asked_at')
+    .select('contexts.name as league_name')
+    .limit(ASK_LIMIT + 1)
+    .execute()
+
+  if (params.p || params.page) {
+    records.reverse()
+  }
+
+  return records
+}
+
+export const getFinanceUserAsks = async (
+  userId: string,
+  params: { n?: Date; p?: Date; page?: string }
+) => {
+  let query = db
+    .selectFrom('finance_asks_users')
+    .innerJoin(
+      'finance_asks',
+      'finance_asks.id',
+      'finance_asks_users.finance_ask_id'
+    )
+    .where('finance_asks.is_in_index', '=', true)
+    .where('finance_asks_users.user_id', '=', userId)
+
+  if (params.n) {
+    query = query
+      .where('finance_asks_users.last_asked_at', '<', params.n)
+      .orderBy('finance_asks_users.last_asked_at', 'desc')
+  } else if (params.p) {
+    query = query
+      .where('finance_asks_users.last_asked_at', '>', params.p)
+      .orderBy('finance_asks_users.last_asked_at', 'asc')
+      .offset(1)
+  } else if (params.page && params.page === 'last') {
+    query = query.orderBy('finance_asks_users.last_asked_at', 'asc')
+  } else {
+    query = query.orderBy('finance_asks_users.last_asked_at', 'desc')
+  }
+
+  const records = await query
+    .selectAll('finance_asks')
+    .select('finance_asks_users.last_asked_at as last_asked_at')
+    .limit(ASK_LIMIT + 1)
+    .execute()
+
+  if (params.p || params.page) {
+    records.reverse()
+  }
+
+  return records
+}
