40c57fcb786cdccebce0d191c7ded1bed157e9a0
adamelmore <elmore.adam@gmail.com>
2023-09-21 07:04:04 -0500

    fix: image broken

--- stacks/img.ts
+++ stacks/img.ts
@@ -186,7 +186,6 @@ export function ImageOptimization({ stack }: StackContext) {
     distribution.addBehavior('app/media/*.jpg', imageOrigin, params)
     distribution.addBehavior('app/media/*.jpeg', imageOrigin, params)
     distribution.addBehavior('app/media/*.png', imageOrigin, params)
-    distribution.addBehavior('app/images/*', imageOrigin, params)
     distribution.addBehavior('finance/asset_img/*', imageOrigin, params)
   }
 
