4a4d090515fe63497d4cc9f737300510ce92b6c5
Eric Perez <ericricardoperez@gmail.com>
2023-09-22 09:57:27 -0700

    bind click event on button

--- packages/web/src/components/cookie-consent.svelte
+++ packages/web/src/components/cookie-consent.svelte
@@ -1,8 +1,18 @@
 <script lang="ts">
   import { session } from "@lib/session-store";
+
+  const consent = () => {
+    fetch(`/auth/cookie-consent`, {
+      method: 'POST',
+      body: JSON.stringify({ cookie_status: 'allow' })
+    })
+    display = false
+  }
+
+  let display = true
 </script>
 
-{#if $session && $session.type !== 'public' && $session.properties.cookieStatus !== 'allow' }
+{#if display && $session && $session.type !== 'public' && $session.properties.cookieStatus !== 'allow' }
   <div role="dialog" aria-live="polite" aria-label="cookieconsent" aria-describedby="cookieconsent:desc" class="bg-[#eaf7f7] text-[#5c7291] flex justify-between items-center px-8 py-4 fixed bottom-0 left-0 w-full z-[5]">
     <!--googleoff: all-->
     <span id="cookieconsent:desc" style="line-height: 1.1em; margin-bottom: 0;">
@@ -12,7 +22,7 @@
       </a>.
     </span>
     <div style="margin-left: 15px;">
-      <a href="#allow" aria-label="allow cookies" tabindex="0" class="bg-[#00c1d8] text-white block py-2 px-4 cursor-pointer">
+      <a href="#allow" on:click|preventDefault={consent} aria-label="allow cookies" tabindex="0" class="bg-[#00c1d8] text-white block py-2 px-4 cursor-pointer">
         Allow
       </a>
     </div>
