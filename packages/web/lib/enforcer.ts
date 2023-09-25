c65e363c6722ae1002080b937bc8ee521382e045
Eric Perez <ericricardoperez@gmail.com>
2023-09-25 14:15:25 -0700

    canQuery helper fn

---
+++ packages/web/lib/enforcer.ts
@@ -0,0 +1,28 @@
+import * as Asks from '@statmuse/core/ask'
+
+const freeAskLimit = 20
+
+export const canQuery = async (locals: App.Locals) => {
+  console.log('locals.subscribed: ', locals.subscribed)
+  if (locals.subscribed) return locals.subscribed
+
+  const result = locals.user
+    ? await Asks.countByUser(locals.user.id)
+    : await Asks.countByVisitor(locals.visitor.id)
+  const count = result ? Number(result.ask_count) : 0
+
+  console.log('count: ', count)
+
+  return count <= freeAskLimit
+}
+
+export const canQueryFinance = async (locals: App.Locals) => {
+  if (locals.subscribed) return locals.subscribed
+
+  const result = locals.user
+    ? await Asks.financeCountByUser(locals.user.id)
+    : await Asks.financeCountByVisitor(locals.visitor.id)
+  const count = result ? Number(result.ask_count) : 0
+
+  return count <= freeAskLimit
+}
