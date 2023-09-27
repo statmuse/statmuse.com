6d85711ce267e134563cb1925619120ebedbd1c1
Eric Perez <ericricardoperez@gmail.com>
2023-09-27 12:20:11 -0700

    get short codes for asks from query

--- packages/core/src/link/index.ts
+++ packages/core/src/link/index.ts
@@ -1,1 +1,23 @@
+import { db } from '../db'
 export * from './link.sql'
+
+export const askShortCode = async (query: string, domain: string) =>
+  db
+    .selectFrom('asks')
+    .innerJoin('links', 'links.ask_id', 'asks.id')
+    .where('asks.query', '=', query)
+    .where(
+      'asks.context_id',
+      '=',
+      db.selectFrom('contexts').where('name', '=', domain).select('id'),
+    )
+    .select('links.short_code')
+    .executeTakeFirst()
+
+export const financeAskShortCode = async (query: string) =>
+  db
+    .selectFrom('finance_asks')
+    .innerJoin('links', 'links.finance_ask_id', 'finance_asks.id')
+    .where('finance_asks.query', '=', query)
+    .select('links.short_code')
+    .executeTakeFirst()
