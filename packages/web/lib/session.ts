622751da979e525ff099cecaa8c9f9201fe2362d
adamelmore <elmore.adam@gmail.com>
2023-09-28 08:17:51 -0500

    chore: more cookie logging

--- packages/web/lib/session.ts
+++ packages/web/lib/session.ts
@@ -22,7 +22,10 @@ export type Context =
 export const get = ({ cookies }: Context) => {
   const cookie = cookies.get(SESSION_COOKIE)?.value
   console.log('cookie', cookie)
-  if (!cookie) return undefined
+  if (!cookie) {
+    console.log(cookies)
+    return undefined
+  }
   const verified = builder.verify(cookie)
   console.log('verified', verified)
   return verified
