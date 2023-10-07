6342f91297586ac26ee0bb2bdc32757fd95c1023
Eric Perez <ericricardoperez@gmail.com>
2023-10-06 17:24:44 -0700

    track html requests

--- packages/functions/src/analytics/track.ts
+++ packages/functions/src/analytics/track.ts
@@ -19,6 +19,19 @@ const analytics = () =>
     writeKey: Config.SEGMENT_WRITE_KEY,
   }).on('error', console.error)
 
+const parseCookie = (cookieString: string) => {
+  const cookieObj = {} as { [key: string]: string | undefined }
+  if (!cookieString) return cookieObj
+
+  const cookiePairs = cookieString.split('; ')
+  for (const pair of cookiePairs) {
+    const [key, value] = pair.split('=')
+    cookieObj[key] = value
+  }
+
+  return cookieObj
+}
+
 export const handler = Handler('kinesis_stream', async (event, ctx) => {
   const incomplete: string[] = event.Records.map((r) => r.eventID).reverse()
 
@@ -37,44 +50,50 @@ export const handler = Handler('kinesis_stream', async (event, ctx) => {
       timeTaken,
       userAgent,
       referrer,
-      cookie,
+      cookieString,
       resultType,
       contentType,
       country,
-      headers,
+      headerString,
       asn,
     ] = parts
 
-    try {
-      await new Promise((resolve) =>
-        analytics().track(
-          {
-            timestamp: new Date(Number.parseFloat(timestamp) * 1000),
-            event: 'CDN Request',
-            // userId: '',
-            anonymousId: randomUUID(),
-            properties: {
-              headers,
-              cookie,
-              uri,
-              contentType,
-              country,
+    const cookies = parseCookie(decodeURIComponent(cookieString))
+
+    if (contentType === 'text/html') {
+      try {
+        await new Promise((resolve) =>
+          analytics().track(
+            {
+              timestamp: new Date(Number.parseFloat(timestamp) * 1000),
+              event: 'CDN Request',
+              userId: cookies['ajs_user_id'],
+              anonymousId: cookies['ajs_anonymous_id'] || randomUUID(),
+              properties: {
+                headers: decodeURIComponent(headerString),
+                cookie: decodeURIComponent(cookieString),
+                uri,
+                contentType,
+                country,
+                method,
+                status,
+              },
+              context: {
+                ip,
+                // @ts-ignore
+                page: uri,
+                // @ts-ignore
+                referrer,
+                userAgent,
+              },
             },
-            context: {
-              ip,
-              // @ts-ignore
-              page: uri,
-              // @ts-ignore
-              referrer,
-              userAgent,
-            },
-          },
-          resolve,
-        ),
-      )
-    } catch (error) {
-      console.error(error)
-      continue
+            resolve,
+          ),
+        )
+      } catch (error) {
+        console.error(error)
+        continue
+      }
     }
 
     incomplete.pop()
