39d55c211542c207ebd54b4e1c8d4f4c281f3dc2
perez-e <ericricardoperez@gmail.com>
2023-10-18 15:38:40 -0700

    Twitter Share (#57)
    * create twitter share icon component
    
    * render twitter share in header
    
    * render twitter share in hero for mobile
    
    * enable native mobile sharing mechanism
    
    * remove shareable bool prop

--- packages/web/lib/props.ts
+++ packages/web/lib/props.ts
@@ -1,9 +1,9 @@
 export type HeroProps = {
   content: string
-  shareable?: boolean
   imageUrl?: string
   imageAlt?: string
   answered?: string
   markdown?: boolean
   audioUrl?: string
+  shareUrl?: string
 }
