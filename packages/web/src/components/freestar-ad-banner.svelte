059434f5532b956ca24064ffefc0eaa3a023c802
Eric Perez <ericricardoperez@gmail.com>
2023-11-18 00:23:56 -0800

    move script into html head

--- packages/web/src/components/freestar-ad-banner.svelte
+++ packages/web/src/components/freestar-ad-banner.svelte
@@ -3,20 +3,7 @@
   export let enableFreestarAdBanner: boolean
 </script>
 
-{#if import.meta.env.PROD && !window.freestar && enableFreestarAdBanner && (($session?.type === 'user' && $session?.properties.subscriptionStatus !== 'active') || ($session?.type === 'visitor' && !$session?.properties.bot))}
-  <script data-cfasync="false" type="text/javascript">
-    var freestar = freestar || {}
-    freestar.queue = freestar.queue || []
-    freestar.config = freestar.config || {}
-    freestar.config.enabled_slots = []
-    freestar.initCallback = function () {
-      if (freestar.config.enabled_slots.length === 0) {
-        freestar.initCallbackCalled = false
-      } else {
-        freestar.newAdSlots(freestar.config.enabled_slots)
-      }
-    }
-  </script>
+{#if import.meta.env.PROD && enableFreestarAdBanner && (($session?.type === 'user' && $session?.properties.subscriptionStatus !== 'active') || ($session?.type === 'visitor' && !$session?.properties.bot))}
   <script
     src="https://a.pub.network/statmuse-com/pubfig.min.js"
     data-cfasync="false"
