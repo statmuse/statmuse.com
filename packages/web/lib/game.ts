8238876be93b914647c86df3462d8c5fb789f867
Eric Perez <ericricardoperez@gmail.com>
2024-01-15 14:10:52 -0800

    get schedule api

--- packages/web/lib/game.ts
+++ packages/web/lib/game.ts
@@ -4,6 +4,7 @@ import type {
   GameraNbaBoxScore,
   GameraNflBoxScore,
   GameraNhlBoxScore,
+  ScheduleResponse,
 } from '@statmuse/core/gamera'
 import { parseGameId } from '@lib/parse'
 import { request } from '@lib/gamera'
@@ -35,6 +36,22 @@ export async function getGame<T extends Domain>(props: {
     const path = `${league}/games/${gameId}/answer`
     const data = await request<Response[T]>(props.context, path, props.params)
     if (data) data.domain = props.domain
+    return data
+  } catch (error) {
+    console.error(error)
+    return undefined
+  }
+}
+
+export const getSchedule = async (props: {
+  context: Context
+  domain: string
+  seasonYear?: number
+}) => {
+  try {
+    const league = props.domain.toLowerCase()
+    const path = `${league}/schedule`
+    const data = await request<ScheduleResponse>(props.context, path)
     return data
   } catch (error) {
     console.error(error)
