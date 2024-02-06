43fa1468ca903be00fefad6149019e2a44a587f5
Eric Perez <ericricardoperez@gmail.com>
2024-02-06 09:55:46 -0800

    create redirect money subdomain stack

---
+++ stacks/redirect.ts
@@ -0,0 +1,73 @@
+import { type StackContext, use } from 'sst/constructs'
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront'
+import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins'
+import * as cert from 'aws-cdk-lib/aws-certificatemanager'
+import * as cdk from 'aws-cdk-lib/core'
+import * as s3 from 'aws-cdk-lib/aws-s3'
+import { DNS } from './dns'
+
+export function RedirectMoney({ stack }: StackContext) {
+  const { hostedZone, domain } = use(DNS)
+
+  if (stack.stage.startsWith('staging') || stack.stage === 'production') {
+    // Define the Lambda@Edge function for redirection
+    const redirectFunction = new lambda.Function(
+      stack,
+      'money-redirect-function',
+      {
+        runtime: lambda.Runtime.NODEJS_18_X,
+        handler: 'index.handler',
+        code: lambda.Code.fromInline(`
+      'use strict';
+      exports.handler = (event, context, callback) => {
+        const response = {
+          status: '302',
+          statusDescription: 'Found',
+          headers: {
+            location: [{
+              key: 'Location',
+              value: 'https://www.statmuse.com/money',
+            }],
+          },
+        };
+        callback(null, response);
+      };
+      `),
+      },
+    )
+
+    const cfDomain =
+      stack.stage === 'production' ? 'money.statmuse.com' : `money.${domain}`
+
+    const cfCert = new cert.Certificate(stack, 'money-cf-cert', {
+      domainName: cfDomain,
+      validation: cert.CertificateValidation.fromDns(hostedZone),
+    })
+
+    const emptyBucket = new s3.Bucket(stack, 'money-redirect-bucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    })
+
+    // Associate the Lambda function with CloudFront distribution
+    const distribution = new cloudfront.Distribution(
+      stack,
+      'money-distribution',
+      {
+        defaultBehavior: {
+          origin: new origins.S3Origin(emptyBucket),
+          viewerProtocolPolicy:
+            cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+          edgeLambdas: [
+            {
+              functionVersion: redirectFunction.currentVersion,
+              eventType: cloudfront.LambdaEdgeEventType.VIEWER_REQUEST,
+            },
+          ],
+        },
+        domainNames: [cfDomain],
+        certificate: cfCert,
+      },
+    )
+  }
+}
