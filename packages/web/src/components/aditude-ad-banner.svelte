1cdc38ebbd9f9c5d92406622cf77839996f9b31b
Eric Perez <ericricardoperez@gmail.com>
2024-02-20 11:00:31 -0800

    create aditude sticky ad banner

---
+++ packages/web/src/components/aditude-ad-banner.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { session } from '@lib/session-store'
+  import { isMobileTest } from '@lib/useragent'
+  import { onMount } from 'svelte'
+
+  let mobile = isMobileTest(navigator.userAgent)
+
+  onMount(() => {
+    window.tude = window.tude || { cmd: [] }
+    tude.cmd.push(function() {
+      tude.refreshAdsViaDivMappings([
+        {
+          divId: 'pb-slot-anchor',
+          baseDivId: 'pb-slot-anchor',
+        }
+      ])
+    })
+  })
+</script>
+
+{#if import.meta.env.PROD && (($session?.type === 'user' && $session?.properties.subscriptionStatus !== 'active') || ($session?.type === 'visitor' && !$session?.properties.bot))}
+  <div class="fixed bottom-0 right-0 w-screen z-50" style:min-height={mobile ? '50px' : '90px'} style:background="#f7f7f7e6">
+    <div id="pb-slot-anchor"></div>
+  </div>
+{/if}
