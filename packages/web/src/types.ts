fdce9032170f00e1fc602a26fc829610a5249192
adamelmore <2363879+adamelmore@users.noreply.github.com>
2024-02-22 07:09:58 -0600

    fix: trendign imports

--- packages/web/src/types.ts
+++ packages/web/src/types.ts
@@ -8,3 +8,25 @@ export const categories = [
 ] as const
 
 export type Category = (typeof categories)[number]
+
+export type TrendingItem = {
+  datasets: [
+    {
+      name: string
+      key: string
+      prominent?: boolean
+      items: TrendingListItem[]
+    },
+  ]
+  updated: string
+}
+
+export interface TrendingListItem {
+  title: string
+  html?: string
+  uri: string
+  images: string[]
+  background: string
+  foreground: string
+  count: number
+}
