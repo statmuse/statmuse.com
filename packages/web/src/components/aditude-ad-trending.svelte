7ec7674f6537cf8421c2ccaa99d7b41e182d4367
Eric Perez <ericricardoperez@gmail.com>
2024-02-27 15:01:59 -0800

    anochor ads in trending page

---
+++ packages/web/src/components/aditude-ad-trending.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+  import { session } from '@lib/session-store'
+  import { isMobileTest } from '@lib/useragent'
+  import { onMount } from 'svelte'
+
+  export let slotId = 'statmuse_incontent_cards'
+
+  let container: HTMLElement
+  let observer: IntersectionObserver
+  let mobile = isMobileTest(navigator.userAgent)
+
+  onMount(() => {
+    return () => {
+      if (observer) {
+        observer.unobserve(container)
+      }
+      if (window.tude) {
+        tude.cmd.push(() => {
+          tude.destroyAds([slotId])
+        })
+      }
+    }
+  })
+
+  $: {
+    if ('IntersectionObserver' in window && mobile && container) {
+      const options = {
+        root: null,
+        rootMargin: '50px',
+        threshold: 0,
+      }
+
+      const callback = (
+        entries: IntersectionObserverEntry[],
+        self: IntersectionObserver,
+      ) => {
+        entries.forEach((entry) => {
+          window.tude = window.tude || { cmd: [] }
+          if (entry.isIntersecting && container) {
+            tude.cmd.push(function () {
+              tude.refreshAdsViaDivMappings([
+                {
+                  divId: slotId,
+                  baseDivId: 'pb-slot-anchor',
+                },
+              ])
+            })
+            self.unobserve(entry.target)
+          }
+        })
+      }
+
+      observer = new IntersectionObserver(callback, options)
+
+      observer.observe(container)
+    }
+  }
+</script>
+
+{#if import.meta.env.PROD && mobile && (($session?.type === 'user' && $session?.properties.subscriptionStatus !== 'active') || ($session?.type === 'visitor' && !$session?.properties.bot))}
+  <div bind:this={container} class="min-h-[50px] p-4 flex justify-center">
+    <div id={slotId}></div>
+  </div>
+{/if}
