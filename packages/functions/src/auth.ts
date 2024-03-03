710ccd3534c84b299a5eefb17d6b54c6fd6a4da6
adamelmore <2363879+adamelmore@users.noreply.github.com>
2024-03-03 04:54:19 -0600

    fix: cors error in auth

--- packages/functions/src/auth.ts
+++ packages/functions/src/auth.ts
@@ -37,6 +37,7 @@ export const handler = AuthHandler({
                 email: claims.email,
                 update: claims.update,
               }).toString(),
+            'Access-Control-Allow-Origin': '*',
           },
         }
       },
@@ -46,6 +47,7 @@ export const handler = AuthHandler({
           headers: {
             Location:
               process.env.AUTH_FRONTEND_URL + '/auth/code?error=invalid_code',
+            'Access-Control-Allow-Origin': '*',
           },
         }
       },
