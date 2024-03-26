25fec022960c259b6b473191f54e91365d4e28e5
Eric Perez <ericricardoperez@gmail.com>
2024-03-26 11:57:19 -0700

    new slot banner ad component

---
+++ packages/web/src/components/aditude-ad-banner.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import { session } from '@lib/session-store'
+  import { isMobileTest } from '@lib/useragent'
+  import { afterUpdate, onMount } from 'svelte'
+
+  let mobile = isMobileTest(navigator.userAgent)
+
+  let renderAd = false
+
+  onMount(() => {
+    return () => {
+      if (window.tude) {
+        tude.cmd.push(() => {
+          tude.destroyAds(['pb-slot-banner'])
+        })
+      }
+    }
+  })
+
+  afterUpdate(() => {
+    if (isNotSubscriber && !renderAd) {
+      window.tude = window.tude || { cmd: [] }
+      tude.cmd.push(function () {
+        tude.refreshAdsViaDivMappings([
+          {
+            divId: 'pb-slot-banner',
+            baseDivId: 'pb-slot-banner',
+          },
+        ])
+      })
+      renderAd = true
+    }
+  })
+
+  $: isNotSubscriber =
+    ($session?.type === 'user' &&
+      $session?.properties.subscriptionStatus !== 'active') ||
+    ($session?.type === 'visitor' && !$session?.properties.bot)
+</script>
+
+{#if import.meta.env.PROD && mobile && isNotSubscriber}
+  <div id="pb-slot-banner"></div>
+{/if}
