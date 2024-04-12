f0d7fac75ab036d0e912ef65668e5cc7513134e9
Eric Perez <ericricardoperez@gmail.com>
2024-04-12 12:38:01 -0700

    upgrade astro + sst deps

--- packages/web/astro.config.mjs
+++ packages/web/astro.config.mjs
@@ -16,7 +16,7 @@ export default defineConfig({
   ],
   output: 'server',
   server: { port: 3000 },
-  // devToolbar: { enabled: false },
+  devToolbar: { enabled: false },
   adapter: aws({
     responseMode: 'buffer',
     serverRoutes: ['ask', 'money/ask', 'fantasy/ask', 'auth/*', 'account/*'],
