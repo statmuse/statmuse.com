3bba9a32c050731fc56c4a9d6343d0e1d0178108
Eric Perez <ericricardoperez@gmail.com>
2024-04-16 18:57:06 -0700

    lockdown with botpoison

--- stacks/web.ts
+++ stacks/web.ts
@@ -128,6 +128,7 @@ export function Web({ stack }: StackContext) {
       auth.privateKey,
       api.api,
       secrets.BOTPOISON_PUBLIC_KEY,
+      secrets.BOTPOISON_SECRET_KEY,
       secrets.STRIPE_SECRET,
       secrets.STRIPE_WEBHOOK_SECRET,
       secrets.STRIPE_PRICE_ID,
