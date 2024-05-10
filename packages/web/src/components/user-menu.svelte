aab2d2b6c860391bc041710a549b2edaad36e90c
perez-e <ericricardoperez@gmail.com>
2024-05-10 10:12:01 -0700

    Miscellaneous Redesign TODOs (#95)
    * css tweaks to auth forms
    
    * redirect home if logged in
    
    * fix spacing in epl boxscore hero
    
    * remove uppercase in grid head cells
    
    * edit example epl queries
    
    * edits to premier league data coverage
    
    * dark stylize user menu
    
    * do not uppercase grid column header cell
    
    * shorten main links in profiles
    
    * render aggregation rows regardless of limit
    
    * fix ordering of players in player gallery
    
    * use player lastname on answer card
    
    * use hero on product pages
    
    * dark mode stylize sidebar nav
    
    * add top nav between product pages
    
    * use base layout in product layout component
    
    * fix user account layout
    
    * clean up
    
    * use hero and nav props from layout
    
    * dark mode on alphabet nav
    
    * create component for player lead cards section
    
    * create component for team rankings cards section
    
    * create component for player fantasy leaders cards section
    
    * create component for team betting cards section
    
    * create component for scores & schedule cards section
    
    * create component for standings cards section
    
    * create component for team specific cards section
    
    * clean up and use card section components
    
    * move card components into cards dir
    
    * pass domain into answer cards component
    
    * fix links and labels
    
    * remove color prop from certian card components
    
    * update fc data glossary
    
    * dark bg on user menu item hover
    
    * Big chances missed (BCM)

--- packages/web/src/components/user-menu.svelte
+++ packages/web/src/components/user-menu.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { createDropdownMenu, melt } from '@melt-ui/svelte'
   import { fly } from 'svelte/transition'
+  import Panel from '@components/panel.svelte'
 
   const {
     elements: { trigger, menu, item },
@@ -32,22 +33,22 @@
 
 {#if $open}
   <div
-    class="flex max-h-[300px] min-w-[220px] flex-col shadow-md z-20
-           rounded-md bg-white py-1 lg:max-h-none
+    class="flex max-h-[300px] min-w-[220px] flex-col shadow-md z-[101]
+           rounded-md bg-gray-8 dark:bg-gray-3 py-1 lg:max-h-none
            ring-1 ring-gray-1 ring-opacity-5"
     use:melt={$menu}
     transition:fly={{ duration: 150, y: -10 }}
   >
     <a
       href="/account"
-      class="z-30 block px-4 py-2 text-sm text-gray-700 no-underline hover:no-underline data-[highlighted]:text-gray-900 data-[highlighted]:bg-gray-100 data-[disabled]:text-neutral-300 ring-0"
+      class="z-30 block px-4 py-2 text-sm text-gray-700 dark:text-inherit no-underline hover:no-underline data-[highlighted]:text-gray-900 data-[highlighted]:bg-gray-100 dark:data-[highlighted]:text-inherit dark:data-[highlighted]:bg-gray-2 data-[disabled]:text-neutral-300 ring-0"
       use:melt={$item}
     >
       Your Account
     </a>
     <a
       href="/auth/sign-out"
-      class="z-30 block px-4 py-2 text-sm text-gray-700 no-underline hover:no-underline data-[highlighted]:text-gray-900 data-[highlighted]:bg-gray-100 data-[disabled]:text-neutral-300 ring-0"
+      class="z-30 block px-4 py-2 text-sm text-gray-700 dark:text-inherit no-underline hover:no-underline data-[highlighted]:text-gray-900 data-[highlighted]:bg-gray-100 dark:data-[highlighted]:text-inherit dark:data-[highlighted]:bg-gray-2 data-[disabled]:text-neutral-300 ring-0"
       use:melt={$item}
       data-astro-reload
       data-astro-prefetch="false"
