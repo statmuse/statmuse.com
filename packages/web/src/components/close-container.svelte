cf9e4e5884350453a541796438c68fe9d7bbac23
Eric Perez <ericricardoperez@gmail.com>
2024-05-20 10:14:14 -0700

    create closeable container

---
+++ packages/web/src/components/close-container.svelte
@@ -0,0 +1,30 @@
+<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<script lang="ts">
+  import { session } from '@lib/stores'
+
+  let isOpen = true
+
+  const onClickClose = () => {
+    isOpen = false
+  }
+
+  console.log('DEV: ', import.meta.env.DEV)
+
+  $: isNotSubscriber =
+    ($session?.type === 'user' &&
+      $session?.properties.subscriptionStatus !== 'active') ||
+    ($session?.type === 'visitor' && !$session?.properties.bot)
+</script>
+
+{#if import.meta.env.PROD && isNotSubscriber && isOpen}
+  <div {...$$props}>
+    <slot />
+    <img
+      src="/share-icons/x.svg"
+      class="w-4 h-4 absolute top-0 right-0 cursor-pointer box-content p-0.5"
+      alt="close"
+      on:click={onClickClose}
+    />
+  </div>
+{/if}
