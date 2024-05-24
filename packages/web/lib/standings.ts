d58d054ce0b2bc52f0577dd846e8463137ad9f66
perez-e <ericricardoperez@gmail.com>
2024-05-24 13:06:50 -0700

    Redesign Home (#97)
    * fetch latest musings by domain
    
    * musing card component
    
    * render musings on first column of home page
    
    * use player card endpoint for home leader card
    
    * render league leaders
    
    * use team card endpoint for home rankings card
    
    * render team rankings
    
    * render trending players, teams and searches
    
    * tweak trending list ui
    
    * render example questions
    
    * render betting trends card
    
    * do the team conference/league grouping in api call
    
    * render standings card
    
    * create player performance card ui
    
    * render player performance card in home
    
    * create abstract ciomponent for leaders and rankings cards
    
    * create abstract aditude display banner component
    
    * use new aditude display banner component
    
    * create closeable container
    
    * use closeable container with aditude component to make sticky ad
    
    * delete aditude components
    
    * create home standings component
    
    * fix and style tabs
    
    * tab ui to flip between musings and trending content on mobile
    
    * fix PGA landing page
    
    * render musings and content cards on slash route
    
    * render skyscraper display ad in right gutter
    
    * render placeholder display ad in dev env
    
    * render med rec ad banner above sky scraper
    
    * add tab title and group standings by division
    
    * light mode tabs
    
    * include set of player illustrations in team ranking cards
    
    * tweak trending cards
    
    * interleave recent musings across leagues for home page
    
    * include team forground color on stat bubble
    
    * use panel component for placeholder ads
    
    * animate home content toggle
    
    * store content selection state and show appropriate content based on selection
    
    * light mode on content switch
    
    * create subscription conditional renderer component
    
    * conditionally render trending content if user is subscriber in third column
    
    * create reusable home content component
    
    * refactor home pages
    
    * remove lazy feature from video player
    
    * render video player at the bottom of home page
    
    * rename stn video player
    
    * replace image with icon component
    
    * add mobile only flag
    
    * rename to closeable container
    
    * collapse all async calls into a single promise
    
    * title trending performace
    
    * revert representatives changes
    
    * move up standings in the list
    
    * make switch smaller
    
    * make images consistent size
    
    * fill in missing leaders and rankings data

--- packages/web/lib/standings.ts
+++ packages/web/lib/standings.ts
@@ -1,6 +1,7 @@
-import type { StandingsResponse } from '@statmuse/core/gamera'
+import type { GameraDomain, StandingsResponse } from '@statmuse/core/gamera'
 import { request } from '@lib/gamera'
 import type { Context } from '@lib/session'
+import { groupBy } from 'lodash-es'
 
 export const getStandings = async (props: {
   context: Context
@@ -11,9 +12,23 @@ export const getStandings = async (props: {
     const league = props.domain.toLowerCase()
     const path = `${league}/standings`
     const data = await request<StandingsResponse>(props.context, path)
-    return data
+    if (data) {
+      return {
+        ...data,
+        domain: props.domain as GameraDomain,
+        teams:
+          league === 'epl'
+            ? { all: data.teams }
+            : groupBy(
+                data.teams,
+                league === 'mlb' ? 'league.name' : 'conference.name',
+              ),
+      }
+    }
   } catch (error) {
     console.error(error)
     return undefined
   }
 }
+
+export type GetStandingsResponse = Awaited<ReturnType<typeof getStandings>>
--- packages/web/src/components/aditude-ad-banner.svelte
+++ packages/web/src/components/aditude-ad-banner.svelte
@@ -1,31 +1,73 @@
 <script lang="ts">
   import { session } from '@lib/stores'
+  import { isMobileTest } from '@lib/useragent'
   import { afterUpdate, onMount } from 'svelte'
+  import Panel from '@components/panel.svelte'
 
-  let renderAd = false
+  export let divId: string
+  export let slotId: string
+  export let onlyMobile = false
+  export let onlyDesktop = false
+  export let lazy = false
+  export let placeholderClass
+
+  const renderAd = () => {
+    window.tude = window.tude || { cmd: [] }
+    tude.cmd.push(function () {
+      tude.refreshAdsViaDivMappings([
+        {
+          divId: divId,
+          baseDivId: slotId,
+        },
+      ])
+    })
+  }
+
+  const isMobile = isMobileTest(navigator.userAgent)
+  const shouldRender = onlyMobile ? isMobile : onlyDesktop ? !isMobile : true
+
+  let container: HTMLElement
+  let observer: IntersectionObserver
+
+  const observerOptions = {
+    root: null,
+    rootMargin: '50px',
+    threshold: 0,
+  }
+
+  const observerCallback = (
+    entries: IntersectionObserverEntry[],
+    self: IntersectionObserver,
+  ) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting && container) {
+        renderAd()
+        self.unobserve(entry.target)
+      }
+    })
+  }
 
   onMount(() => {
     return () => {
+      if (lazy && observer) {
+        observer.unobserve(container)
+      }
       if (window.tude) {
         tude.cmd.push(() => {
-          tude.destroyAds(['pb-slot-anchor'])
+          tude.destroyAds([divId])
         })
       }
     }
   })
 
   afterUpdate(() => {
-    if (isNotSubscriber && !renderAd) {
-      window.tude = window.tude || { cmd: [] }
-      tude.cmd.push(function () {
-        tude.refreshAdsViaDivMappings([
-          {
-            divId: 'pb-slot-anchor',
-            baseDivId: 'pb-slot-anchor',
-          },
-        ])
-      })
-      renderAd = true
+    if (isNotSubscriber && shouldRender) {
+      if (lazy && container && 'IntersectionObserver' in window) {
+        observer = new IntersectionObserver(observerCallback, observerOptions)
+        observer.observe(container)
+      } else {
+        renderAd()
+      }
     }
   })
 
@@ -35,6 +77,15 @@
     ($session?.type === 'visitor' && !$session?.properties.bot)
 </script>
 
-{#if import.meta.env.PROD && isNotSubscriber}
-  <div id="pb-slot-anchor"></div>
+{#if shouldRender && isNotSubscriber}
+  {#if import.meta.env.PROD}
+    <div {...$$restProps} bind:this={container} id={divId}></div>
+  {:else}
+    <Panel
+      {...$$restProps}
+      class={`${placeholderClass} flex items-center justify-center`}
+    >
+      Display Ad
+    </Panel>
+  {/if}
 {/if}
