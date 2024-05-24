351574040556330094eb969da0ed72b918cbeaf9
Eric Perez <ericricardoperez@gmail.com>
2024-05-24 01:51:26 -0700

    replace image with icon component

--- packages/web/src/components/close-container.svelte
+++ packages/web/src/components/close-container.svelte
@@ -2,6 +2,7 @@
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <script lang="ts">
   import { session } from '@lib/stores'
+  import Icon from '@components/icon.svelte'
 
   let isOpen = true
 
@@ -18,10 +19,9 @@
 {#if isNotSubscriber && isOpen}
   <div {...$$props}>
     <slot />
-    <img
-      src="/share-icons/x.svg"
+    <Icon
+      name="x"
       class="w-4 h-4 absolute top-0 right-0 cursor-pointer box-content p-0.5"
-      alt="close"
       on:click={onClickClose}
     />
   </div>
