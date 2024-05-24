8e60c203632f7afeb78c5bfafc09edf91e511373
Eric Perez <ericricardoperez@gmail.com>
2024-05-23 21:58:31 -0700

    create subscription conditional renderer component

---
+++ packages/web/src/components/with-subscription.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { session } from '@lib/stores'
+
+  $: isSubscriber =
+    $session?.type === 'user' &&
+    $session.properties.subscriptionStatus === 'active'
+</script>
+
+{#if $session}
+  {#if isSubscriber}
+    <slot name="subscribed" />
+  {:else}
+    <slot />
+  {/if}
+{/if}
