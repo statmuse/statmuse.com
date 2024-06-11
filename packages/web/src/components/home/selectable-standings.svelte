68971bb8cfb0c01e9cc0d1061590d96e50d2a16a
Eric Perez <ericricardoperez@gmail.com>
2024-06-10 19:36:03 -0700

    new design for standings card

--- packages/web/src/components/home/selectable-standings.svelte
+++ packages/web/src/components/home/selectable-standings.svelte
@@ -9,6 +9,7 @@
   } from '@melt-ui/svelte'
 
   export let options: SelectOption<string>[]
+  export let toggle: Record<string, string[]>
 
   const defaultSelected = options[0]
   let selectedValue: string = defaultSelected.value
@@ -38,39 +39,62 @@
     },
     onSelectedChange: handleChange,
   })
+
+  $: toggleOptions = toggle[selectedValue]
 </script>
 
-<Panel class="!pb-0 !px-0">
-  <button
-    use:melt={$trigger}
-    class=" w-56 px-2 text-left flex justify-between items-center"
-  >
-    {$selectedLabel
-      ? `${$selectedLabel} ${
-          $selectedLabel === 'Premier Leaegue' ? 'Table' : 'Standings'
-        }`
-      : 'Choose an option'}
-    {#if $open}
-      <Icon name="dropdown-up" class="w-4 h-4" />
-    {:else}
-      <Icon name="dropdown" class="w-4 h-4" />
-    {/if}
-  </button>
-  {#if $open}
-    <div
-      use:melt={$menu}
-      class="bg-gray-6 dark:bg-gray-3 rounded-b-2xl overflow-clip"
+<Panel class="!pb-0 !px-0 group">
+  <div class="flex justify-between mb-1">
+    <button
+      use:melt={$trigger}
+      class="font-semibold w-40 px-3 text-left flex justify-between items-center"
     >
-      {#each options as league}
-        <p
-          use:melt={$option(league)}
-          class="data-[highlighted]:dark:bg-gray-1 px-2 cursor-pointer"
-        >
-          {league.label}
-        </p>
-      {/each}
-    </div>
-  {/if}
+      {$selectedLabel
+        ? `${$selectedLabel} ${$selectedLabel === 'PL' ? 'Table' : 'Standings'}`
+        : 'Choose an option'}
+      {#if $open}
+        <Icon name="dropdown-up" class="w-4 h-4" />
+      {:else}
+        <Icon name="dropdown" class="w-4 h-4" />
+      {/if}
+    </button>
+    {#if $open}
+      <div
+        use:melt={$menu}
+        class="bg-gray-6 dark:bg-gray-3 rounded-b-2xl overflow-clip"
+      >
+        {#each options as league}
+          <p
+            use:melt={$option(league)}
+            class="data-[highlighted]:bg-gray-7 data-[highlighted]:dark:bg-gray-2 px-3 cursor-pointer"
+          >
+            {league.label}
+          </p>
+        {/each}
+      </div>
+    {/if}
+    {#if toggleOptions}
+      <div
+        class="bg-gray-8 dark:bg-gray-4 text-inherit dark:text-gray-5 border border-gray-6 dark:border-gray-3 flex text-center rounded-full relative overflow-clip mr-3"
+      >
+        <div
+          class="absolute h-full w-1/2 bg-teal dark:bg-gray-7 rounded-full left-0 group-has-[#standings-0:checked]:translate-x-0 group-has-[#standings-1:checked]:translate-x-full transition-transform ease-out"
+        />
+        {#each toggleOptions as option, i}
+          <label
+            for={`standings-${i}`}
+            class="py-1 w-12 relative"
+            class:group-has-[#standings-0:checked]:text-gray-8={i == 0}
+            class:dark:group-has-[#standings-0:checked]:text-gray-2={i == 0}
+            class:group-has-[#standings-1:checked]:text-gray-8={i == 1}
+            class:dark:group-has-[#standings-1:checked]:text-gray-2={i == 1}
+          >
+            {option}
+          </label>
+        {/each}
+      </div>
+    {/if}
+  </div>
   {#if selectedValue === 'epl'}
     <slot name="epl" />
   {:else if selectedValue === 'nba'}
