69aaef8375d71af6803481f8cd49bbb2c87f8afa
Eric Perez <ericricardoperez@gmail.com>
2024-06-20 14:58:01 -0700

    bigger font in user menu

--- packages/web/src/components/user-menu.svelte
+++ packages/web/src/components/user-menu.svelte
@@ -24,21 +24,21 @@
 {#if $open}
   <div
     class="flex max-h-[300px] min-w-[220px] flex-col shadow-md z-[101]
-           rounded-md bg-gray-8 dark:bg-gray-3 py-1 lg:max-h-none
+           rounded-xl bg-gray-8 dark:bg-gray-3 py-1 lg:max-h-none
            ring-1 ring-gray-1 ring-opacity-5"
     use:melt={$menu}
     transition:fly={{ duration: 150, y: -10 }}
   >
     <a
       href="/account"
-      class="z-30 block px-4 py-2 text-sm text-gray-700 dark:text-inherit no-underline hover:no-underline data-[highlighted]:text-gray-900 data-[highlighted]:bg-gray-100 dark:data-[highlighted]:text-inherit dark:data-[highlighted]:bg-gray-2 data-[disabled]:text-neutral-300 ring-0"
+      class="z-30 block px-4 py-2 text-gray-700 dark:text-inherit no-underline hover:no-underline data-[highlighted]:text-gray-900 data-[highlighted]:bg-gray-100 dark:data-[highlighted]:text-inherit dark:data-[highlighted]:bg-gray-2 data-[disabled]:text-neutral-300 ring-0"
       use:melt={$item}
     >
       Your Account
     </a>
     <a
       href="/auth/sign-out"
-      class="z-30 block px-4 py-2 text-sm text-gray-700 dark:text-inherit no-underline hover:no-underline data-[highlighted]:text-gray-900 data-[highlighted]:bg-gray-100 dark:data-[highlighted]:text-inherit dark:data-[highlighted]:bg-gray-2 data-[disabled]:text-neutral-300 ring-0"
+      class="z-30 block px-4 py-2 text-gray-700 dark:text-inherit no-underline hover:no-underline data-[highlighted]:text-gray-900 data-[highlighted]:bg-gray-100 dark:data-[highlighted]:text-inherit dark:data-[highlighted]:bg-gray-2 data-[disabled]:text-neutral-300 ring-0"
       use:melt={$item}
       data-astro-reload
       data-astro-prefetch="false"
