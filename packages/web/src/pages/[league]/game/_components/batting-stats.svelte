3a3dcf5e03a62767c9db098dd000fdb7e25b7ac4
Eric Perez <ericricardoperez@gmail.com>
2024-08-02 16:17:34 -0700

    add batting summary

--- packages/web/src/pages/[league]/game/_components/batting-stats.svelte
+++ packages/web/src/pages/[league]/game/_components/batting-stats.svelte
@@ -102,6 +102,89 @@
       }
     })
     .filter((x) => !!x)
+
+  const doubles = splits
+    .filter((s) => s.stats?.['Batting-Doubles']?.value > 0)
+    .map((s) => {
+      const player = playerMap[s.playerId]
+      return {
+        name: player.entity.shortDisplay,
+        total: s.stats?.['Batting-Doubles']?.value,
+      }
+    })
+  const triples = splits
+    .filter((s) => s.stats?.['Batting-Triples']?.value > 0)
+    .map((s) => {
+      const player = playerMap[s.playerId]
+      return {
+        name: player.entity.shortDisplay,
+        total: s.stats?.['Batting-Triples']?.value,
+      }
+    })
+  const homeRun = splits
+    .filter((s) => s.stats?.['Batting-HomeRuns']?.value > 0)
+    .map((s) => {
+      const player = playerMap[s.playerId]
+      return {
+        name: player.entity.shortDisplay,
+        total: s.stats?.['Batting-HomeRuns']?.value,
+      }
+    })
+  const rbis = splits
+    .filter((s) => s.stats?.['Batting-RunsBattedIn']?.value > 0)
+    .map((s) => {
+      const player = playerMap[s.playerId]
+      return {
+        name: player.entity.shortDisplay,
+        total: s.stats?.['Batting-RunsBattedIn']?.value,
+      }
+    })
 </script>
 
-<Grid data={{ columns, rows }} stickyColumns={['NAME']} />
+<div class="space-y-3">
+  <Grid data={{ columns, rows }} stickyColumns={['NAME']} />
+
+  <div class="text-sm">
+    <p class="mb-1.5">Batting</p>
+    {#if doubles.length > 0}
+      <p>
+        2B:
+        <span class="text-gray-5">
+          {doubles
+            .map((p) => `${p.name}${p?.total > 1 ? ` ${p.total}` : ''}`)
+            .join(', ')}
+        </span>
+      </p>
+    {/if}
+    {#if triples.length > 0}
+      <p>
+        3B:
+        <span class="text-gray-5">
+          {triples
+            .map((p) => `${p.name}${p?.total > 1 ? ` ${p.total}` : ''}`)
+            .join(', ')}
+        </span>
+      </p>
+    {/if}
+    {#if homeRun.length > 0}
+      <p>
+        HR:
+        <span class="text-gray-5">
+          {homeRun
+            .map((p) => `${p.name}${p?.total > 1 ? ` ${p.total}` : ''}`)
+            .join(', ')}
+        </span>
+      </p>
+    {/if}
+    {#if rbis.length > 0}
+      <p>
+        RBI:
+        <span class="text-gray-5">
+          {rbis
+            .map((p) => `${p.name}${p?.total > 1 ? ` ${p.total}` : ''}`)
+            .join(', ')}
+        </span>
+      </p>
+    {/if}
+  </div>
+</div>
