49f38ed29dd88fb8da12809687aedd61c73bfb6b
Eric Perez <ericricardoperez@gmail.com>
2024-08-08 14:04:18 -0700

    finish bus target game event handler

--- packages/functions/src/game-event.ts
+++ packages/functions/src/game-event.ts
@@ -1,3 +1,75 @@
-export const handler = (event) => {
-  console.log(event)
+import { Config } from 'sst/node/config'
+import type {
+  MlbGameDataResponse,
+  MlbPlayByPlayResponse,
+} from '@statmuse/core/gamera'
+import * as Realtime from '@statmuse/core/realtime'
+
+const gameraApiUrl = process.env.GAMERA_API_URL
+const gameraApiKey = Config.GAMERA_API_KEY
+
+const headers: Record<string, string> = {
+  Accept: 'application/json',
+  'x-origin': 'game-event.bot',
+  'x-origin-scope': 'game-event',
+  'x-origin-version': '1.0',
+  'x-visitor-id': 'game-event',
+  'x-statmuse-api-key': gameraApiKey,
+}
+
+type StatKey = 'pitchingStandard' | 'battingStandard'
+
+const fetchGameData = async (props: {
+  gameId: string | number
+  statKeySet: StatKey[]
+  version?: string
+}): Promise<MlbGameDataResponse | undefined> => {
+  try {
+    const { gameId, statKeySet } = props
+    const requestUrl =
+      `${gameraApiUrl}mlb/games/${gameId}?` +
+      new URLSearchParams(
+        statKeySet.map((key) => ['statKeySet', key]),
+      ).toString()
+    const response = await fetch(requestUrl, { headers })
+    return response.json()
+  } catch (error) {
+    console.error(error)
+    return undefined
+  }
+}
+
+const fetchPlayByPlay = async (props: {
+  gameId: string | number
+  version?: string
+}): Promise<MlbPlayByPlayResponse | undefined> => {
+  try {
+    const { gameId, ...params } = props
+    const requestUrl = `${gameraApiUrl}mlb/games/${gameId}/playByPlay?${new URLSearchParams(
+      params,
+    ).toString()}`
+    const response = await fetch(requestUrl, { headers })
+    return response.json()
+  } catch (error) {
+    console.error(error)
+    return undefined
+  }
+}
+
+export const handler = async (event: any) => {
+  try {
+    const { gameId, gameVersion: version } = event.detail
+    // const [gameData, playByPlay] = await Promise.all([
+    //   fetchGameData({
+    //     gameId,
+    //     version,
+    //     statKeySet: ['battingStandard', 'pitchingStandard'],
+    //   }),
+    //   fetchPlayByPlay({ gameId, version }),
+    // ])
+
+    Realtime.publish(`mlb/game/${gameId}/all`, { data: 'sample-data' })
+  } catch (error) {
+    console.error(error)
+  }
 }
--- stacks/game-event-bus.ts
+++ stacks/game-event-bus.ts
@@ -1,9 +1,23 @@
-import { EventBus } from 'sst/constructs'
+import { EventBus, use } from 'sst/constructs'
 import type { StackContext } from 'sst/constructs'
 import * as eventbus from 'aws-cdk-lib/aws-events'
+import { API } from './api'
+import { Secrets } from './secrets'
 
 export function GameEventBus({ stack }: StackContext) {
+  const api = use(API)
+  const secrets = use(Secrets)
+
   const bus = new EventBus(stack, 'game-bus', {
+    defaults: {
+      function: {
+        bind: [secrets.GAMERA_API_KEY],
+        permissions: ['iot'],
+        environment: {
+          GAMERA_API_URL: api.environment.GAMERA_API_URL,
+        },
+      },
+    },
     rules: {
       gameUpdate: {
         pattern: {
