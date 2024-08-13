d202d77d933335f8b2d6c717cd2c882896bb7a96
Eric Perez <ericricardoperez@gmail.com>
2024-08-13 13:36:22 -0700

    reverse order of half inning for live all plays

--- packages/web/src/pages/[league]/game/_components/play-by-play.svelte
+++ packages/web/src/pages/[league]/game/_components/play-by-play.svelte
@@ -4,27 +4,49 @@
   import type { GameraTeamReference } from '@statmuse/core/gamera/index'
 
   export let scoring: boolean = false
+  export let reverseOrder: boolean = false
   export let awayTeam: GameraTeamReference | undefined = undefined
   export let homeTeam: GameraTeamReference | undefined = undefined
 
-  $: inningPlays = scoring ? $scoringInnings : $innings
+  $: inningPlays = scoring
+    ? $scoringInnings
+    : reverseOrder
+    ? $innings.toReversed()
+    : $innings
 </script>
 
 {#each inningPlays as inning (inning)}
   {@const number = inning.number}
   {@const [topHalf, bottomHalf] = inning.halves}
-  {#if topHalf.events.length > 0}
-    <HalfInningPlayByPlay
-      inningTitle={`Top ${number}`}
-      inning={topHalf}
-      colors={awayTeam?.colors}
-    />
-  {/if}
-  {#if bottomHalf.events.length > 0}
-    <HalfInningPlayByPlay
-      inningTitle={`Bottom ${number}`}
-      inning={bottomHalf}
-      colors={homeTeam?.colors}
-    />
+  {#if reverseOrder}
+    {#if bottomHalf.events.length > 0}
+      <HalfInningPlayByPlay
+        inningTitle={`Bottom ${number}`}
+        inning={bottomHalf}
+        colors={homeTeam?.colors}
+      />
+    {/if}
+    {#if topHalf.events.length > 0}
+      <HalfInningPlayByPlay
+        inningTitle={`Top ${number}`}
+        inning={topHalf}
+        colors={awayTeam?.colors}
+      />
+    {/if}
+  {:else}
+    {#if topHalf.events.length > 0}
+      <HalfInningPlayByPlay
+        inningTitle={`Top ${number}`}
+        inning={topHalf}
+        colors={awayTeam?.colors}
+      />
+    {/if}
+    {#if bottomHalf.events.length > 0}
+      <HalfInningPlayByPlay
+        inningTitle={`Bottom ${number}`}
+        inning={bottomHalf}
+        colors={homeTeam?.colors}
+      />
+    {/if}
   {/if}
 {/each}
