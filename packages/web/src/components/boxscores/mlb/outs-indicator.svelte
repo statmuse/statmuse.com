5333d13e99a24e1522a15093f9310cbaea5292d8
Eric Perez <ericricardoperez@gmail.com>
2024-09-07 21:22:10 -0700

    thicker border on outs indicator

--- packages/web/src/components/boxscores/mlb/outs-indicator.svelte
+++ packages/web/src/components/boxscores/mlb/outs-indicator.svelte
@@ -1,19 +1,23 @@
 <script lang="ts">
   export let outs: number
   export let vertical: boolean | undefined = undefined
+  export let thickerBorder = false
 </script>
 
 <div class={`flex gap-1 ${$$props.class}`} class:flex-col={vertical}>
   <div
     class="rounded-full border border-current w-2 h-2"
+    class:border-2={thickerBorder}
     class:bg-current={outs > 0}
   />
   <div
     class="rounded-full border border-current w-2 h-2"
+    class:border-2={thickerBorder}
     class:bg-current={outs > 1}
   />
   <div
     class="rounded-full border border-current w-2 h-2"
+    class:border-2={thickerBorder}
     class:bg-current={outs > 2}
   />
 </div>
