e0e4d123a2fc637a9bd52bf46e8a2db8f5c6f7ba
Eric Perez <ericricardoperez@gmail.com>
2024-09-10 14:51:02 -0700

    realtime component for scores page

---
+++ packages/web/src/components/scores/realtime.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import Realtime from '@components/realtime.svelte'
+  import * as stores from './stores'
+  import type { GameraDomain } from '@statmuse/core/gamera'
+
+  type ConnectionParam = {
+    domain: GameraDomain
+    gameId: number
+    topic: string
+  }
+
+  export let connectionParams: ConnectionParam[]
+</script>
+
+<Realtime
+  {connectionParams}
+  subscriptionHandler={(_topic, payload) => {
+    stores.addMlbGame(payload)
+  }}
+/>
--- packages/web/src/components/scores/stores.ts
+++ packages/web/src/components/scores/stores.ts
@@ -1,49 +1,12 @@
-import {
-  getCurrentHalfInning,
-  getCurrentInningNumber,
-  getCurrentOuts,
-  getCurrentRunners,
-  type GameraDomain,
-  type MlbGameDataResponse,
-  type MlbPlayByPlayResponse,
-  type MlbStatKey,
-  type Runner,
-} from '@statmuse/core/gamera'
+import type { MlbGameScore } from '@statmuse/core/gamera'
 import { atom } from 'nanostores'
 
-type MlbGame = {
-  score: {
-    away: number
-    home: number
-  }
-  inningNumber: number
-  inningHalf: 'top' | 'bottom'
-  outs: number
-  runners: Runner[]
-}
+export const scores = atom<Record<string, MlbGameScore | undefined>>({})
 
-export const scores = atom<Record<string, MlbGame | undefined>>({})
-
-export const addGame = (
-  domain: GameraDomain,
-  game: {
-    data?: MlbGameDataResponse<MlbStatKey>
-    playByPlay?: MlbPlayByPlayResponse
-  },
-) => {
+export const addMlbGame = (game: MlbGameScore) => {
   const allGames = scores.get()
   scores.set({
     ...allGames,
-    [`${domain}-${game.data?.gameId ?? 0}`]: {
-      score: {
-        away: game.data?.awayTeam.score ?? 0,
-        home: game.data?.homeTeam.score ?? 0,
-      },
-      inningHalf:
-        getCurrentHalfInning(game.playByPlay?.innings ?? [])?.half ?? 'top',
-      inningNumber: getCurrentInningNumber(game.playByPlay?.innings ?? []),
-      outs: getCurrentOuts(game.playByPlay?.innings ?? []),
-      runners: getCurrentRunners(game.playByPlay?.innings ?? []),
-    },
+    [`${game.domain}-${game.gameId}`]: game,
   })
 }
