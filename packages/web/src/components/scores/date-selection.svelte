a3c1afa7a6ede8fc7c3595523a19807a6b8e599c
Eric Perez <ericricardoperez@gmail.com>
2024-09-13 20:16:38 -0700

    fix date timezone issues

---
+++ packages/web/src/components/scores/date-selection.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+  import dayjs from 'dayjs'
+  import { onMount } from 'svelte'
+
+  export let days: string[]
+  export let selectedDate: string | null
+  export let basePath: string
+
+  let container: HTMLElement
+
+  onMount(() => {
+    const parent = container.getBoundingClientRect()
+    const selected = container.querySelector<HTMLElement>('[data-selected]')
+
+    if (selected) {
+      const elem = selected.getBoundingClientRect()
+      container.scrollLeft =
+        elem.left - parent.left + (elem.width - parent.width) / 2
+    }
+  })
+</script>
+
+<div
+  bind:this={container}
+  class={`flex gap-3 overflow-x-scroll no-scrollbar ${$$props.class}`}
+>
+  {#each days as day (day)}
+    {@const date = dayjs(day)}
+    <div
+      class="group/day-nav flex flex-col gap-1.5 overflow-clip shrink-0"
+      {...day === selectedDate
+        ? { 'data-selected': '' }
+        : date.date() === dayjs().date()
+        ? { 'data-selected': '' }
+        : {}}
+    >
+      <a
+        href={`${basePath}?${new URLSearchParams({
+          date: date.format('YYYY-MM-DD'),
+        }).toString()}`}
+        class="text-inherit hover:no-underline"
+      >
+        {date.date() === dayjs().date()
+          ? 'Today'
+          : date.date() === dayjs().subtract(1, 'day').date()
+          ? 'Yesterday'
+          : date.date() === dayjs().add(1, 'day').date()
+          ? 'Tomorrow'
+          : date.format('ddd MMM D')}
+      </a>
+      <div
+        class={`hidden w-full h-5 -mb-2.5 bg-current rounded-2xl ${
+          day !== selectedDate ? 'md:group-hover/day-nav:block' : ''
+        }`}
+        class:!block={day === selectedDate ||
+          (selectedDate === null && date.date() === dayjs().date())}
+      />
+    </div>
+  {/each}
+</div>
--- packages/web/src/components/scores/day-navigation.svelte
+++ packages/web/src/components/scores/day-navigation.svelte
@@ -1,55 +1,38 @@
 <script lang="ts">
+  import Icon from '@components/icon.svelte'
   import dayjs from 'dayjs'
-  import { onMount } from 'svelte'
 
-  export let days: string[]
-  export let selectedDate: string
+  export let selectedDate: string | null
   export let basePath: string
 
-  let container: HTMLElement
-
-  onMount(() => {
-    const parent = container.getBoundingClientRect()
-    const selected = container.querySelector<HTMLElement>('[data-selected]')
-
-    if (selected) {
-      const elem = selected.getBoundingClientRect()
-      container.scrollLeft =
-        elem.left - parent.left + (elem.width - parent.width) / 2
-    }
-  })
+  const date = selectedDate ? dayjs(selectedDate) : undefined
+  const today = dayjs()
 </script>
 
 <div
-  bind:this={container}
-  class={`flex gap-3 overflow-x-scroll no-scrollbar ${$$props.class}`}
+  class="w-32 py-0.5 px-2 rounded-2xl flex justify-between items-center bg-team-secondary text-team-primary"
 >
-  {#each days as day (day)}
-    {@const date = dayjs(day)}
-    <div
-      class="group/day-nav flex flex-col gap-1.5 overflow-clip shrink-0"
-      {...day === selectedDate ? { 'data-selected': '' } : {}}
-    >
-      <a
-        href={`${basePath}?${new URLSearchParams({
-          date: date.format('YYYY-MM-DD'),
-        }).toString()}`}
-        class="text-inherit hover:no-underline"
-      >
-        {date.date() === dayjs().date()
-          ? 'Today'
-          : date.date() === dayjs().subtract(1, 'day').date()
-          ? 'Yesterday'
-          : date.date() === dayjs().add(1, 'day').date()
-          ? 'Tomorrow'
-          : date.format('ddd MMM D')}
-      </a>
-      <div
-        class={`w-full h-5 -mb-2.5 bg-current rounded-2xl ${
-          day !== selectedDate ? 'md:group-hover/day-nav:block' : ''
-        }`}
-        class:hidden={day !== selectedDate}
-      />
-    </div>
-  {/each}
+  <a
+    href={`${basePath}?${new URLSearchParams({
+      date: (date ?? today).subtract(1, 'day').format('YYYY-MM-DD'),
+    }).toString()}`}
+    class="text-inherit"
+  >
+    <Icon name="next" class="w-4 h-4 rotate-180" />
+  </a>
+  {selectedDate === null || date?.date() === today.date()
+    ? 'Today'
+    : date?.date() === today.subtract(1, 'day').date()
+    ? 'Yesterday'
+    : date?.date() === today.add(1, 'day').date()
+    ? 'Tomorrow'
+    : date?.format('ddd MMM D')}
+  <a
+    href={`${basePath}?${new URLSearchParams({
+      date: (date ?? today).add(1, 'day').format('YYYY-MM-DD'),
+    }).toString()}`}
+    class="text-inherit"
+  >
+    <Icon name="next" class="w-4 h-4" />
+  </a>
 </div>
