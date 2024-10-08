3188187e22b78789cd08de4c0b38d49723211396
Eric Perez <ericricardoperez@gmail.com>
2024-10-08 16:30:45 -0700

    create game state component to serialize game data & play by play

---
+++ packages/web/src/components/boxscores/nfl/game-state.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import type {
+    NflGameDataResponse,
+    NflPlayByPlayResponse,
+    NflStatKey,
+  } from '@statmuse/core/gamera/index'
+  import * as store from './stores'
+  import { onMount } from 'svelte'
+
+  export let gameData: NflGameDataResponse<NflStatKey>
+  export let playByPlay: NflPlayByPlayResponse
+
+  onMount(() => {
+    store.init({ gameData, playByPlay })
+    // return () => store.gameState.set({})
+  })
+</script>
