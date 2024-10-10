155b51a162ff7a4b7c050bbb23962e4ce4c89b31
Eric Perez <ericricardoperez@gmail.com>
2024-10-10 13:02:26 -0700

    extend injuries api fn to accept domain parameter

--- packages/web/lib/gamera/injuries.ts
+++ packages/web/lib/gamera/injuries.ts
@@ -1,12 +1,27 @@
 import { request } from '@lib/gamera/base'
-import type { TeamInjuryResponse } from '@statmuse/core/gamera'
+import type {
+  GameraDomain,
+  InjuriesResponseByDomain,
+} from '@statmuse/core/gamera'
 import type { Context } from '@lib/session'
 
-export const getInjuries = async (props: { context: Context }) => {
+export const getInjuries = async <
+  D extends Extract<GameraDomain, 'MLB' | 'NFL'>,
+>(props: {
+  context: Context
+  domain: D
+  teamId?: number | number[]
+}) => {
   try {
-    const data = await request<TeamInjuryResponse>(
-      props.context,
-      'mlb/injuries',
+    const { domain, context, ...params } = props
+    const data = await request<InjuriesResponseByDomain[D]>(
+      context,
+      `${domain.toLowerCase()}/injuries`,
+      new URLSearchParams(
+        Object.entries(params)
+          .map(([k, v]) => (Array.isArray(v) ? v.map((x) => [k, x]) : [[k, v]]))
+          .flat() as string[][],
+      ),
     )
     return data
   } catch (error) {
